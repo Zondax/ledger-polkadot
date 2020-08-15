cc8b897928e282af6234f5b657f9d3de00c2cbde
Juan Leni <juan.leni@zondax.ch>
2020-08-15 22:38:32 +0200

    upgrade zxlib

--- deps/ledger-zxlib/app/common/view_x.c
+++ deps/ledger-zxlib/app/common/view_x.c
@@ -72,33 +72,19 @@ UX_FLOW(
 );
 
 ///////////
+
 UX_STEP_INIT(ux_review_flow_2_start_step, NULL, NULL, { h_review_loop_start(); });
 UX_STEP_NOCB_INIT(ux_review_flow_2_step, bnnn_paging, { h_review_loop_inside(); }, { .title = viewdata.key, .text = viewdata.value, });
 UX_STEP_INIT(ux_review_flow_2_end_step, NULL, NULL, { h_review_loop_end(); });
+UX_STEP_VALID(ux_review_flow_3_step, pb, h_approve(0), { &C_icon_validate_14, "Approve" });
+UX_STEP_VALID(ux_review_flow_4_step, pb, h_reject(0), { &C_icon_crossmark, "Reject" });
 
-UX_STEP_NOCB(ux_sign_flow_1_step, pbb, { &C_icon_eye, "View", "Transaction" });
-UX_STEP_VALID(ux_sign_flow_3_step, pbb, h_sign_accept(0), { &C_icon_validate_14, "Sign", "Transaction" });
-UX_STEP_VALID(ux_sign_flow_4_step, pbb, h_sign_reject(0), { &C_icon_crossmark, "Reject", "Transaction" });
-
-const ux_flow_step_t *const ux_sign_flow[] = {
-  &ux_sign_flow_1_step,
+const ux_flow_step_t *const ux_review_flow[] = {
   &ux_review_flow_2_start_step,
   &ux_review_flow_2_step,
   &ux_review_flow_2_end_step,
-  &ux_sign_flow_3_step,
-  &ux_sign_flow_4_step,
-  FLOW_END_STEP,
-};
-
-UX_STEP_NOCB(ux_address_flow_1_step, pbb, { &C_icon_eye, "View", "Address" });
-UX_STEP_VALID(ux_address_flow_3_step, pbb, h_address_accept(0), { &C_icon_validate_14, "Accept", "Address" });
-
-const ux_flow_step_t *const ux_address_flow[] = {
-  &ux_address_flow_1_step,
-  &ux_review_flow_2_start_step,
-  &ux_review_flow_2_step,
-  &ux_review_flow_2_end_step,
-  &ux_address_flow_3_step,
+  &ux_review_flow_3_step,
+  &ux_review_flow_4_step,
   FLOW_END_STEP,
 };
 
@@ -212,19 +198,7 @@ void view_idle_show_impl(uint8_t item_idx) {
     ux_flow_init(0, ux_idle_flow, NULL);
 }
 
-void view_address_show_impl() {
-    h_paging_init();
-    h_paging_decrease();
-
-    ////
-    flow_inside_loop = 0;
-    if(G_ux.stack_count == 0) {
-        ux_stack_push();
-    }
-    ux_flow_init(0, ux_address_flow, NULL);
-}
-
-void view_sign_show_impl(){
+void view_review_show_impl(){
     h_paging_init();
     h_paging_decrease();
     ////
@@ -232,7 +206,7 @@ void view_sign_show_impl(){
     if(G_ux.stack_count == 0) {
         ux_stack_push();
     }
-    ux_flow_init(0, ux_sign_flow, NULL);
+    ux_flow_init(0, ux_review_flow, NULL);
 }
 
 void view_error_show_impl() {
@@ -242,5 +216,4 @@ void view_error_show_impl() {
     }
     ux_flow_init(0, ux_error_flow, NULL);
 }
-
 #endif
