bf5fb174506aa91345de8ff77d0e5f8205e0a0d9
Fernando Theirs <fntheirs@gmail.com>
2022-03-10 23:00:16 +0100

    add nanos+ device

--- tests_zemu/tests/common.ts
+++ tests_zemu/tests/common.ts
@@ -6,8 +6,10 @@ export const APP_SEED = 'equip will roof matter pink blind book anxiety banner e
 
 const APP_PATH_S = Resolve('../app/output/app_s.elf')
 const APP_PATH_X = Resolve('../app/output/app_x.elf')
+const APP_PATH_SP = Resolve('../app/output/app_s2.elf')
 
 export const models: DeviceModel[] = [
   { name: 'nanos', prefix: 'S', path: APP_PATH_S },
   { name: 'nanox', prefix: 'X', path: APP_PATH_X },
+  { name: 'nanosp', prefix: 'SP', path: APP_PATH_SP },
 ]
