b386ac8a5268c3ed425217be6980f930d284dce7
Carlo Sala <carlosalag@protonmail.com>
2022-05-18 19:19:49 +0200

    support prefix > 63

--- app/src/crypto_helper.h
+++ app/src/crypto_helper.h
@@ -30,8 +30,10 @@ extern "C" {
 #define SS58_BLAKE_PREFIX_LEN 7
 #define SS58_ADDRESS_MAX_LEN 60u
 
+uint8_t crypto_SS58CalculatePrefix(uint16_t addressType, uint8_t *prefixBytes);
+
 uint8_t crypto_SS58EncodePubkey(uint8_t *buffer, uint16_t buffer_len,
-                                uint8_t addressType, const uint8_t *pubkey);
+                                uint16_t addressType, const uint8_t *pubkey);
 
 #ifdef __cplusplus
 }
