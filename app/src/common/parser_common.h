08615520ddf6e05968bc10511c26faceb6dce342
Carlo Sala <carlosalag@protonmail.com>
2023-01-19 20:05:56 +0100

    update copyright year

--- app/src/common/parser_common.h
+++ app/src/common/parser_common.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-*  (c) 2019 - 2022  Zondax AG
+*  (c) 2019 - 2023  Zondax AG
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
