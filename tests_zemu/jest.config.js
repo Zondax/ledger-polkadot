af2de9c8654de537d54fe85a705771e613ac90fb
Andrés Benso <andresbenso@gmail.com>
2025-12-10 09:07:50 -0300

    Use standard Blake3 library (#274)
    * use standard blake3 library
    
    * bump version
    
    * use zondax fork to blake3

--- tests_zemu/jest.config.js
+++ tests_zemu/jest.config.js
@@ -4,4 +4,6 @@ module.exports = {
   transformIgnorePatterns: ['^.+\\.js$'],
   reporters: ['default', ['summary', { summaryThreshold: 1 }]],
   globalSetup: './globalsetup.js',
+  // Retry flaky tests up to 2 times (3 attempts total)
+  retryTimes: 2,
 }
